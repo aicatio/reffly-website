fe907ba1504258ff2b7c298cdd79d554da8fab67
iamrashed01 <irashad42@gmail.com>
2022-11-15 08:21:13 +0600

    feate: added donation section

---
+++ src/components/buyMeaCoffe/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import carbonad from "../../assets/carbonad-v1.png";
+
+type Props = { className?: string };
+
+export default function BuyMeaCoffe({ className }: Props) {
+  return (
+    <div className={`${className}`}>
+      <div className="flex gap-4 justify-center items-center flex-row">
+        <div>
+          <img className="max-w-[300px]" src={carbonad} alt="" />
+        </div>
+        <div>
+          <p>Fund us on -</p>
+          <img
+            className="max-w-[300px]"
+            src="https://cdn.buymeacoffee.com/assets/img/home-page-v3/bmc-new-logo.png"
+            alt="buy me a coffe"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { HeadFC, PageProps } from "gatsby";
 import * as React from "react";
 import Banner from "../components/banner";
+import BuyMeaCoffe from "../components/buyMeaCoffe";
 import Layout from "../components/layouts/layouts";
 import UrlShortnerForm from "../components/urlShortnerForm";
 
@@ -8,7 +9,8 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <Banner className="mt-10" />
-      <UrlShortnerForm className="mt-16" />
+      <UrlShortnerForm className="mt-12" />
+      <BuyMeaCoffe className="mt-10" />
     </Layout>
   );
 };
